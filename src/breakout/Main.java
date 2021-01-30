dafc8203551e35ec5aa4b7ea151c6266fd839483
Jiyang Tang <jt304@duke.edu>
2021-01-30 13:57:32 +0800

    Borrow lab_bounce code and data files

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,13 +1,156 @@
 package breakout;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
 /**
- * Feel free to completely change this code or delete it entirely. 
+ * A basic example JavaFX program for the first lab.
+ *
+ * @author Robert C. Duvall
  */
-public class Main {
-  /**
-   * Start of the program.
-   */
-  public static void main(String[] args) {
-    System.out.println("Hello world");
-  }
+public class Main extends Application {
+    public static final String TITLE = "Breakout";
+    public static final int SIZE = 400;
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    public static final Paint BACKGROUND = Color.AZURE;
+    public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
+    public static final String BOUNCER_IMAGE = "ball.gif";
+    public static final int BOUNCER_SPEED = 30;
+    public static final Paint MOVER_COLOR = Color.PLUM;
+    public static final int MOVER_SIZE = 50;
+    public static final int MOVER_SPEED = 5;
+    public static final Paint GROWER_COLOR = Color.BISQUE;
+    public static final double GROWER_RATE = 1.1;
+    public static final int GROWER_SIZE = 50;
+
+    // some things needed to remember during game
+    private Scene myScene;
+    private ImageView myBouncer;
+    private Rectangle myMover;
+    private Rectangle myGrower;
+
+
+    /**
+     * Initialize what will be displayed and how it will be updated.
+     */
+    @Override
+    public void start(Stage stage) {
+        // attach scene to the stage and display it
+        myScene = setupGame(SIZE, SIZE, BACKGROUND);
+        stage.setScene(myScene);
+        stage.setTitle(TITLE);
+        stage.show();
+        // attach "game loop" to timeline to play it (basically just calling step() method repeatedly forever)
+        KeyFrame frame = new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step(SECOND_DELAY));
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    // Create the game's "scene": what shapes will be in the game and their starting properties
+    private Scene setupGame(int width, int height, Paint background) {
+        // create one top level collection to organize the things in the scene
+        Group root = new Group();
+        // make some shapes and set their properties
+        Image image = new Image(this.getClass().getClassLoader().getResourceAsStream(BOUNCER_IMAGE));
+        myBouncer = new ImageView(image);
+        // x and y represent the top left corner, so center it in window
+        myBouncer.setX(width / 2 - myBouncer.getBoundsInLocal().getWidth() / 2);
+        myBouncer.setY(height / 2 - myBouncer.getBoundsInLocal().getHeight() / 2);
+        myMover = new Rectangle(width / 2 - MOVER_SIZE / 2, height / 2 - 100, MOVER_SIZE, MOVER_SIZE);
+        myMover.setFill(MOVER_COLOR);
+        myGrower = new Rectangle(width / 2 - GROWER_SIZE / 2, height / 2 + 50, GROWER_SIZE, GROWER_SIZE);
+        myGrower.setFill(GROWER_COLOR);
+        // order added to the group is the order in which they are drawn
+        root.getChildren().add(myBouncer);
+        root.getChildren().add(myMover);
+        root.getChildren().add(myGrower);
+        // create a place to see the shapes
+        Scene scene = new Scene(root, width, height, background);
+        // respond to input
+        scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+        scene.setOnMouseClicked(e -> handleMouseInput(e.getX(), e.getY()));
+        return scene;
+    }
+
+    // Change properties of shapes in small ways to animate them over time
+    // Note, there are more sophisticated ways to animate shapes, but these simple ways work fine to start
+    private void step(double elapsedTime) {
+        // update "actors" attributes
+        myBouncer.setX(myBouncer.getX() + BOUNCER_SPEED * elapsedTime);
+        myMover.setRotate(myMover.getRotate() - 1);
+        myGrower.setRotate(myGrower.getRotate() + 1);
+
+        // check for collisions
+        //   with shapes, can check precisely
+        // NOTE Java 10 syntax that simplifies things (but watch out it can make code harder to understand)
+        // var intersection = Shape.intersect(myMover, myGrower);
+        Shape intersection = Shape.intersect(myMover, myGrower);
+        if (intersection.getBoundsInLocal().getWidth() != -1) {
+            myMover.setFill(HIGHLIGHT);
+        } else {
+            myMover.setFill(MOVER_COLOR);
+        }
+        //   with images can only check bounding box
+        if (myGrower.getBoundsInParent().intersects(myBouncer.getBoundsInParent())) {
+            myGrower.setFill(HIGHLIGHT);
+        } else {
+            myGrower.setFill(GROWER_COLOR);
+        }
+    }
+
+    // What to do each time a key is pressed
+    private void handleKeyInput(KeyCode code) {
+        // NOTE Java 14 syntax that some prefer (but watch out for the many special cases!)
+        //   https://blog.jetbrains.com/idea/2019/02/java-12-and-intellij-idea/
+        switch (code) {
+            case RIGHT -> myMover.setX(myMover.getX() + MOVER_SPEED);
+            case LEFT -> myMover.setX(myMover.getX() - MOVER_SPEED);
+            case UP -> myMover.setY(myMover.getY() - MOVER_SPEED);
+            case DOWN -> myMover.setY(myMover.getY() + MOVER_SPEED);
+        }
+        // TYPICAL way to do it, definitely more readable for longer actions
+//        if (code == KeyCode.RIGHT) {
+//            myMover.setX(myMover.getX() + MOVER_SPEED);
+//        }
+//        else if (code == KeyCode.LEFT) {
+//            myMover.setX(myMover.getX() - MOVER_SPEED);
+//        }
+//        else if (code == KeyCode.UP) {
+//            myMover.setY(myMover.getY() - MOVER_SPEED);
+//        }
+//        else if (code == KeyCode.DOWN) {
+//            myMover.setY(myMover.getY() + MOVER_SPEED);
+//        }
+    }
+
+    // What to do each time a key is pressed
+    private void handleMouseInput(double x, double y) {
+        if (myGrower.contains(x, y)) {
+            myGrower.setScaleX(myGrower.getScaleX() * GROWER_RATE);
+            myGrower.setScaleY(myGrower.getScaleY() * GROWER_RATE);
+        }
+    }
+
+    /**
+     * Start the program.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
